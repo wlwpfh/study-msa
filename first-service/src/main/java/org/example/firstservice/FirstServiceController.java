50622431494650d57b4e7c1a7a9bbb107ca3b95c
yang Jiyoung <0112jyoung@naver.com>
2025-12-08 20:34:52 +0900

    [section 2] 루트 pom.xml 설정 및 first-service, second-service 모듈 추가

---
+++ first-service/src/main/java/org/example/firstservice/FirstServiceController.java
@@ -0,0 +1,15 @@
+package org.example.firstservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/first-service")
+public class FirstServiceController {
+    @GetMapping("/test")
+    public String test() {
+        return "test first service";
+    }
+
+}
---
+++ gateway-api/src/test/java/org/example/gatewayapi/GatewayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gatewayapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
