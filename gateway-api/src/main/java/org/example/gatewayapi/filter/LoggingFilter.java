0e656c54acd1cf1287c4cdb972fcb2be98833fec
yang Jiyoung <0112jyoung@naver.com>
2025-12-26 23:41:41 +0900

    [section 3] logging filter 적용 - 우선순위 설정

--- gateway-api/src/main/java/org/example/gatewayapi/filter/LoggingFilter.java
+++ gateway-api/src/main/java/org/example/gatewayapi/filter/LoggingFilter.java
@@ -3,7 +3,9 @@ package org.example.gatewayapi.filter;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.Ordered;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
@@ -15,9 +17,32 @@ public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Co
     public LoggingFilter() {
         super(LoggingFilter.Config.class);
     }
+//    @Override
+//    public GatewayFilter apply(Config config) {
+//        return ((exchange, chain) -> {
+//            ServerHttpRequest request = exchange.getRequest();
+//            ServerHttpResponse response = exchange.getResponse();
+//
+//            log.info("Logging PRE filter: baseMessage -> {}, {}", config.getBaseMessage(), request.getRemoteAddress());
+//
+//            if(config.isPreLogger()) {
+//                log.info("Logging PRE filter: request uri -> {}", request.getURI());
+//            }
+//
+//            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+//                if(config.isPostLogger()) {
+//                    log.info("Logging POST end: response code -> {}", response.getStatusCode());
+//                }
+//            }));
+//        });
+//    }
+
+    /**
+     * 우선순위 버전
+    * */
     @Override
     public GatewayFilter apply(Config config) {
-        return ((exchange, chain) -> {
+        return new OrderedGatewayFilter((exchange, chain) -> {
             ServerHttpRequest request = exchange.getRequest();
             ServerHttpResponse response = exchange.getResponse();
 
@@ -32,7 +57,7 @@ public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Co
                     log.info("Logging POST end: response code -> {}", response.getStatusCode());
                 }
             }));
-        });
+        }, Ordered.HIGHEST_PRECEDENCE);
     }
 
     @Data
