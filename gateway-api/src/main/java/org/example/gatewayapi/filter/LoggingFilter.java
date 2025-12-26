084093b0554d14bf9afc225fd8d39328a6078c3c
yang Jiyoung <0112jyoung@naver.com>
2025-12-26 23:35:53 +0900

    [section 3] logging filter 적용

---
+++ gateway-api/src/main/java/org/example/gatewayapi/filter/LoggingFilter.java
@@ -0,0 +1,44 @@
+package org.example.gatewayapi.filter;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Config> {
+    public LoggingFilter() {
+        super(LoggingFilter.Config.class);
+    }
+    @Override
+    public GatewayFilter apply(Config config) {
+        return ((exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            log.info("Logging PRE filter: baseMessage -> {}, {}", config.getBaseMessage(), request.getRemoteAddress());
+
+            if(config.isPreLogger()) {
+                log.info("Logging PRE filter: request uri -> {}", request.getURI());
+            }
+
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                if(config.isPostLogger()) {
+                    log.info("Logging POST end: response code -> {}", response.getStatusCode());
+                }
+            }));
+        });
+    }
+
+    @Data
+    public static class Config{
+        private String baseMessage;
+        private boolean preLogger;
+        private boolean postLogger;
+    }
+}
