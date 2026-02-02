e1c15d980601323183f40b03941d0abcfa08ae31
yang Jiyoung <0112jyoung@naver.com>
2026-02-02 21:47:54 +0900

    [section 8] configuration service - modify a typo - modify pom.xml - add bootstrap.yml

--- gateway-api/src/main/java/org/example/gatewayapi/GatewayApiApplication.java
+++ gateway-api/src/main/java/org/example/gatewayapi/GatewayApiApplication.java
@@ -1,8 +1,11 @@
 package org.example.gatewayapi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +15,8 @@ public class GatewayApiApplication {
 		SpringApplication.run(GatewayApiApplication.class, args);
 	}
 
+	@Bean
+	public HttpExchangeRepository httpExchangeRepository() {
+		return new InMemoryHttpExchangeRepository();
+	}
 }
